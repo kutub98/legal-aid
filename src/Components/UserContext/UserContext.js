5fe71679a0654e960c23dc87c11f8139caa37e01
kutub98 <kutubuddin98710@gmail.com>
2022-11-09 15:51:10 +0600

    addedGoogleAndGitHubHandler

--- src/Components/UserContext/UserContext.js
+++ src/Components/UserContext/UserContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, GithubAuthProvider } from "firebase/auth";
 import app from "../Firebase/FireBaseConfig/FirebaseConfig";
 
 export const AuthContext = createContext();
@@ -16,6 +16,8 @@ const UserContext = ({ children }) => {
   const LoginWithEp = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
+
+  // On State change ==================
   useEffect(() => {
     const unSubscriber = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
@@ -25,7 +27,20 @@ const UserContext = ({ children }) => {
     };
   }, []);
 
-  const authInfo = { auth, user, createWithEp, LoginWithEp};
+  // login with Google
+
+  const GoogleProvider = new GoogleAuthProvider()
+  const loginWithGoogleProvider = () =>{
+    return signInWithPopup(auth, GoogleProvider)
+  }
+
+  // login with git hub======================
+  const GitHubProvider = new GithubAuthProvider()
+  const loginWithGitHub = ()=>{
+    return signInWithPopup(auth, GitHubProvider)
+  }
+
+  const authInfo = { auth, user, createWithEp, LoginWithEp, loginWithGoogleProvider, loginWithGitHub};
   return (
     <div>
       <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
